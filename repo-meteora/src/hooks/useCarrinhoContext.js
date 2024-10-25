659dfff903c19d876994778f3538899a4c82a64b
renatohvo <renatohvo@gmail.com>
2024-10-25 18:17:55 -0300

    feat(react-context-api): valor total e quantidade utilizando reduce

--- repo-meteora/src/hooks/useCarrinhoContext.js
+++ repo-meteora/src/hooks/useCarrinhoContext.js
@@ -1,8 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  const {
+    carrinho,
+    setCarrinho,
+    quantidade,
+    setQuantidade,
+    valorTotal,
+    setValorTotal,
+  } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map((itemCarrinho) => {
@@ -40,11 +47,22 @@ export const useCarrinhoContext = () => {
     setCarrinho(produto);
   }
 
+  useEffect(() => {
+    const { totalTemp, quantidadeTemp } = carrinho.reduce((acumulador, produto) => ({
+      quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+      totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+    }), { quantidadeTemp: 0, totalTemp: 0 });
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  }, [carrinho])
+
   return {
     carrinho,
     setCarrinho,
     adicionarProduto,
     removerProduto,
     removerProdutoCarrinho,
+    valorTotal,
+    quantidade,
   }
 }
