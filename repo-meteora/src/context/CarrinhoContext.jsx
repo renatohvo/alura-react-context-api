e197d459865c15cc7a33662e5ea9bba2a4297cb0
renatohvo <renatohvo@gmail.com>
2024-10-25 17:16:53 -0300

    feat(react-context-api): display name react context dev tools

--- repo-meteora/src/context/CarrinhoContext.jsx
+++ repo-meteora/src/context/CarrinhoContext.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from "react";
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
+  CarrinhoContext.displayName = "Carrinho"
   const [carrinho, setCarrinho] = useState([]);
   return (
     <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
